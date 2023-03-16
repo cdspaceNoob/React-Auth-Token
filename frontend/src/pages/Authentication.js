39e3261ff635f89907ca280f7e46c5b06337b279
voyager <cdspacenoob@gmail.com>
2023-03-16 13:51:40 +0900

    validate form with useActionData

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,3 +1,4 @@
+import { json, redirect } from "react-router-dom";
 import AuthForm from "../components/AuthForm";
 
 const AuthenticationPage = () => {
@@ -5,3 +6,40 @@ const AuthenticationPage = () => {
 };
 
 export default AuthenticationPage;
+
+export const action = async ({ request }) => {
+  // URL은 브라우저 built-in이다.
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login";
+
+  if (mode !== "login" && mode !== "signup") {
+    throw json({ message: "invalid mode" }, { status: 422 });
+  }
+
+  const data = await request.formData();
+  const authData = {
+    // 여기서 get 메소드는 formData의 반환값(데이터)에 들어있다.
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+
+  const response = await fetch("http://localhost:8080/" + mode, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(authData),
+  });
+
+  if (response.status === 422 || response.status === 401) {
+    return response; // 이렇게만 해줘도 ReactRouter가 알아서 데이터를 추출한다.
+  }
+
+  if (!response.ok) {
+    throw json({ message: "could not authenticate user" }, { status: 500 });
+  }
+
+  // token
+
+  return redirect("/");
+};
