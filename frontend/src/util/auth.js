c6b5ad99601ef3ca72314b3285002391cffd7aae
voyager <cdspacenoob@gmail.com>
2023-03-17 10:53:36 +0900

    check if token is and controll code

--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,3 +1,5 @@
+import { redirect } from "react-router-dom";
+
 export const getAuthToken = () => {
   const token = localStorage.getItem("token");
   return token;
@@ -5,4 +7,13 @@ export const getAuthToken = () => {
 
 export const tokenLoader = () => {
   return getAuthToken();
+};
+
+export const checkAuthLoader = () => {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return redirect("/");
+  }
+  return null;
 };
